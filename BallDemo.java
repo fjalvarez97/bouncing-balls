0727968031fb5404e05e5f54ccbc31806ce1703e
fjalvarez97 <franjalvarez955@gmail.com>
2018-02-23 00:40:17 +0100

    Ahora se crean tantas bolas como indiquemos por parametro

--- BallDemo.java
+++ BallDemo.java
@@ -1,5 +1,6 @@
 import java.awt.Color;
-
+import java.util.ArrayList;
+import java.util.Random;
 /**
  * Class BallDemo - a short demonstration showing animation with the 
  * Canvas class. 
@@ -11,19 +12,20 @@ import java.awt.Color;
 public class BallDemo   
 {
     private Canvas myCanvas;
-
+    private ArrayList<BouncingBall> balls;
     /**
      * Create a BallDemo object. Creates a fresh canvas and makes it visible.
      */
     public BallDemo()
     {
         myCanvas = new Canvas("Ball Demo", 600, 500);
+        balls = new ArrayList<>();
     }
 
     /**
      * Simulate two bouncing balls
      */
-    public void bounce()
+    public void bounce(int ballsNumber)
     {
         int ground = 400;   // position of the ground line
 
@@ -33,20 +35,25 @@ public class BallDemo
         myCanvas.drawLine(50, ground, 550, ground);
 
         // crate and show the balls
-        BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
-        ball.draw();
-        BouncingBall ball2 = new BouncingBall(70, 80, 20, Color.RED, ground, myCanvas);
-        ball2.draw();
 
+        for (int i = 0; i < ballsNumber; i++){
+            BouncingBall ball = new BouncingBall(50, 50, 16, Color.BLUE, ground, myCanvas);
+            ball.draw();   
+            balls.add(ball);
+        }
         // make them bounce
         boolean finished =  false;
         while(!finished) {
-            myCanvas.wait(50);           // small delay
-            ball.move();
-            ball2.move();
+            myCanvas.wait(50); 
+            // small delay
+            for (int i=0; i < ballsNumber ; i++){ 
+                balls.get(i).move();
+            }
             // stop once ball has travelled a certain distance on x axis
-            if(ball.getXPosition() >= 550 || ball2.getXPosition() >= 550) {
-                finished = true;
+            for (int i=0; i <ballsNumber ; i++) {
+                if(balls.get(i).getXPosition() >= 550) {
+                    finished = true;
+                }
             }
         }
     }
